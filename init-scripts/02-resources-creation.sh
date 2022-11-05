1a9908d7c8ecb8a6bd9e355277ca101f1fa415ec
Michael Horojanski <mhorojanski@chegg.com>
2022-11-05 15:08:08 +0200

    add

--- init-scripts/02-resources-creation.sh
+++
@@ -1,9 +0,0 @@
-#!bin/bash
-
-echo "-------------------------------------Script-02"
-
-echo "########### Creating SQS ###########"
-# aws sqs create-queue --endpoint-url=http://localhost:4566 --queue-name=pastes --profile localstack
-
-echo "########### Listing SQS ###########"
-# aws sqs list-queues --endpoint-url=http://localhost:4566 --profile localstack
