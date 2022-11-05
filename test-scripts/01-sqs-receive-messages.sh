d3983d9b3faa257db5137a312b0a0ed6ccd28445
Michael Horojanski <mhorojanski@chegg.com>
2022-11-05 15:07:22 +0200

    add

--- test-scripts/01-sqs-receive-messages.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-aws --endpoint-url=http://localhost:4566 sqs receive-message --queue-url http://localhost:4566/000000000000/collect-queue.fifo --profile localstack --attribute-names All --message-attribute-names All --max-number-of-messages 10
