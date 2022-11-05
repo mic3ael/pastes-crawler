2a5163ba641eb48646eef19715196f81d2d4598e
Michael Horojanski <mhorojanski@chegg.com>
2022-11-05 16:05:31 +0200

    increase timeout

--- src/utils/http/client.js
+++ src/utils/http/client.js
@@ -5,11 +5,13 @@ const axios = require('axios');
 const UserAgent = require('user-agents');
 const { ServerError } = require('./errors');
 
+const DEFAULT_TIMEOUT = 10000;
+
 async function get(path) {
   try {
     const userAgent = new UserAgent();
     const resolvedUrl = url.resolve(this, path);
-    const config = { url: resolvedUrl, method: 'get', timeout: 2000 };
+    const config = { url: resolvedUrl, method: 'get', timeout: DEFAULT_TIMEOUT };
     const headers = {
       'User-Agent': userAgent.toString(),
     };
